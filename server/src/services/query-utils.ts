76c3f38f460806a1f78e551edbcf9a3b6ff092fe
liora <liorahron11@gmail.com>
2024-12-20 23:40:56 +0200

    feat(): add initial server (from assignment)

---
+++ server/src/services/query-utils.ts
@@ -0,0 +1,14 @@
+export const stringifyUpdatedUserFields = (isPasswordUpdated: boolean, isUsernameUpdated: boolean, isEmailUpdated: boolean): string => {
+    let updatedFields: string = '';
+    if (isPasswordUpdated) {
+        updatedFields += 'password ';
+    }
+    if (isUsernameUpdated) {
+        updatedFields += 'username ';
+    }
+    if (isEmailUpdated) {
+        updatedFields += 'email ';
+    }
+
+    return updatedFields;
+}
